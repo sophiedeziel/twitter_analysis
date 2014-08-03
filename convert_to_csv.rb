3d4227a310d05e533f90ae01a921b9b85cff5204
Sophie Deziel <courrier@sophiedeziel.com>
2014-08-03 19:54:48 -0400

    classification

--- convert_to_csv.rb
+++ convert_to_csv.rb
@@ -15,21 +15,31 @@ def convert_to_csv(filename)
   i = 0
 
   CSV.open(File.basename(filename, '.*') + '.csv', 'wb') do |csv|
-    csv << %w(id lang created_at source in_reply_to_status_id in_reply_to_user_id in_reply_to_screen_name user_id user_location user_followers_count user_friends_count user_time_zone, user_statuses_count, user_lang, place_full_name, place_country_code, geo_x, geo_y, is_good)
-
+    csv << %w(id lang created_at source in_reply_to_status_id in_reply_to_user_id in_reply_to_screen_name user_id user_location user_followers_count user_friends_count user_time_zone user_statuses_count user_lang place_full_name place_country_code geo_x geo_y is_good)
   end
 
-
+  @pid = nil
 
   tweets.each do |t|
-
+    system("osascript -e 'tell application \"safari\" to quit'")
+    system('clear')
     i += 1
     puts "================================"
-    puts "\ntweet #{i} of #{c}"
+    puts "tweet #{i} of #{c}"
     puts "--------------------------------"
     puts "\n\n#{t['text']} \n\n"
+
+    if t['entities']['media'] && t['entities']['media'].any?
+      @pid = open_link t['entities']['media'].first['media_url']
+    end
+
+    if t['entities']['urls'] && t['entities']['urls'].any?
+      @pid = open_link t['entities']['urls'].first['expanded_url']
+    end
+
     is_good = prompt "Is about the animal?"
     if is_good == 'q'
+      system("osascript -e 'tell application \"safari\" to quit'")
       exit
     end
     CSV.open(File.basename(filename, '.*') + '.csv', 'ab') do |csv|
@@ -44,13 +54,13 @@ def convert_to_csv(filename)
               t['user']['location'],
               t['user']['followers_count'],
               t['user']['friends_count'],
-              t['user']['time_zone'],
+              t['user']['time_zone'] ? t['user']['time_zone'] : nil,
               t['user']['statuses_count'],
               t['user']['lang'],
-              t['place'] ? t['place']['full_name'] : nil,
-              t['place'] ? t['place']['country_code'] : nil,
-              t['geo'] ? t['geo']['coordinates'].first : nil,
-              t['geo'] ? t['geo']['coordinates'].last : nil,
+              t['place'] ? t['place']['full_name'] : '',
+              t['place'] ? t['place']['country_code'] : '',
+              t['geo'] ? t['geo']['coordinates'].first : 0,
+              t['geo'] ? t['geo']['coordinates'].last : 0,
               is_good
       ]
     end
@@ -65,6 +75,11 @@ def prompt message
   c = STDIN.getc
   system("stty -raw echo")
   c
+end
+
+def open_link link
+  pid = Process.spawn( "open -a Safari -g #{link}" )
+  pid
 end
 
 filename = ARGV[0]
